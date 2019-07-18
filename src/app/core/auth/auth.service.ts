d6a512b11f42c20d4f2e9904ee6501fa0a415bcf
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2019-07-18 16:25:13 -0300

    Implementando o Header

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { tap } from 'rxjs/operators';
+import { TokenService } from './token/token.service';
+
 const API_URL = 'http://localhost:3000';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService) { }
 
   authenticate(userName: string, password: string) {
 
-    return this.http.post(API_URL + `/user/login`, { userName, password });
+    return this.http
+            .post(API_URL + `/user/login`, { userName, password }, { observe: 'response'})
+            .pipe(tap(res => {
+              const authToken = res.headers.get('x-access-token');
+              this.tokenService.setToken(authToken);
+              console.log(authToken);
+            }));
   }
 }
